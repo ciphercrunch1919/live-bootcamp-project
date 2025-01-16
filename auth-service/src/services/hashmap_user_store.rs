c0bc463b6cbc9162ab7c896accbc9d9c470c4fbd
Elena Coleman <17coleme@gmail.,com>
2025-01-16 07:01:59 -0700

    [Sprint 2][Task 3} fix: Implement SOLID Design

--- auth-service/src/services/hashmap_user_store.rs
+++ auth-service/src/services/hashmap_user_store.rs
@@ -1,18 +1,44 @@
 use std::collections::HashMap;
 
-use crate::domain::user::User;
-
-#[derive(Debug, PartialEq)]
-pub enum UserStoreError {
-    UserAlreadyExists,
-    UserNotFound,
-    InvalidCredentials,
-    UnexpectedError,
-}
-
+use crate::domain::{user::User, data_stores::{UserStoreError, UserStore}};
 #[derive(Default)]
 pub struct HashmapUserStore {
     users: HashMap<String, User>,
+}
+
+#[async_trait::async_trait]
+impl UserStore for HashmapUserStore {
+    async fn add_user(&mut self, user: User) -> Result<(), UserStoreError> {
+        if self.users.contains_key(&user.email) {
+            return Err(UserStoreError::UserAlreadyExists);
+        }
+        self.users.insert(user.email.clone(), user);
+        return Ok(());
+    }
+
+    async fn get_user(&self, email: &str) -> Result<User, UserStoreError> {
+        match self.users.get(email) {
+            Some(user) => Ok(user.clone()),
+            None => Err(UserStoreError::UserNotFound),
+        }
+    }
+
+    async fn validate_user(
+        &self,
+        email: &str,
+        password: &str,
+    ) -> Result<(), UserStoreError> {
+        match self.users.get(email) {
+            Some(user) => {
+                if user.password.eq(password) {
+                    Ok(())
+                } else {
+                    Err(UserStoreError::InvalidCredentials)
+                }
+            }
+            None => Err(UserStoreError::UserNotFound),
+        }
+    }
 }
 
 impl HashmapUserStore {
